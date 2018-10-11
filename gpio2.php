ff8b2679e1fdbfd5a978f78db57da845a6ce0eba
jason <detourstudios07@gmail.com>
2018-10-11 14:56:55 +0000

    modified:   gpio2.php 	modified:   gpio3.php 	modified:   gpio4.php 	modified:   gpio5.php 	modified:   gpio6.php 	modified:   gpio7.php 	modified:   gpio8.php

--- gpio2.php
+++ gpio2.php
@@ -1,21 +1,21 @@
 
 <?php
-         shell_exec("/usr/local/bin/gpio -g mode 2 out");
+         shell_exec("/usr/local/bin/gpio -g mode 3 out");
          if(isset($_GET['off']))
          {
-                shell_exec("/usr/local/bin/gpio -g write 2 0");
+                shell_exec("/usr/local/bin/gpio -g write 3 0");
          }
         else if(isset($_GET['on']))
         {
-                shell_exec("/usr/local/bin/gpio -g write 2 1");
+                shell_exec("/usr/local/bin/gpio -g write 3 1");
         }
         else if(isset($_GET['blink']))
         {
                 for($x = 0;$x<=4;$x++)
                 {
-                        shell_exec("/usr/local/bin/gpio -g write 2 1");
+                        shell_exec("/usr/local/bin/gpio -g write 3 1");
                         sleep(1);
-                        shell_exec("/usr/local/bin/gpio -g write 2 0");
+                        shell_exec("/usr/local/bin/gpio -g write 3 0");
                         sleep(1);
                 }
         }
