c27240f8a2ec0497ce31a07d3656b5df0871e616
jasonmello114 <detourstudios07@gmail.com>
2018-11-01 12:21:30 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,14 +1,17 @@
 var attempt = 3; // Variable to count number of attempts.
+
 // Below function Executes on click of login button.
 function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
-if ( username == "jason" && password == "Lacee123"){
+
+  if ( username == "jason" && password == "Lacee123"){
 //alert ("Login successfully");
 window.location.href = "kalismarthome.com/dash.html";
 return false;
 }
-else{
+  
+/*else{
 attempt --;// Decrementing by one.
 alert("You have "+attempt+" attempts left;");
 // Disabling fields after 3 attempts.
@@ -16,7 +19,7 @@ if( attempt == 0){
 document.getElementById("username").disabled = true;
 document.getElementById("password").disabled = true;
 document.getElementById("submit").disabled = true;
-return false;
+return false;*/
 }
 }
 }
