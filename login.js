d5a3e6d3c8680eda6e63fd5ecea1eed6ab815999
jasonmello114 <detourstudios07@gmail.com>
2018-11-02 13:08:53 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,16 +1,12 @@
-var attempt = 3; // Variable to count number of attempts.
-
-// Below function Executes on click of login button.
 function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 
   if ( username == "jason" && password == "Lacee123"){
-/*alert ("Login successfully");*/
-window.location.href = "/dash.html";
+window.location.href = "dash.html";
 return false;
 }
-  
+}
 /*else{
 attempt --;// Decrementing by one.
 alert("You have "+attempt+" attempts left;");
