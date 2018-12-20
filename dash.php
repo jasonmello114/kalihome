63c2c4acf3c8f8d0bc2de1ec35dcc83d6317769d
jasonmello114 <detourstudios07@gmail.com>
2018-12-20 11:36:09 -0500

    Update dash.php

--- dash.php
+++ dash.php
@@ -277,7 +277,9 @@
                      </form>
                   </center>
                </div>
-               <p>
+               <div>
+                  
+               </div>
                <!-- Made with love -->
                <div class="mdl-shadow--2dp mdl-cell--12-col" style="background-color:#3c3a3f">
                   <div class="mdl-card-title">
