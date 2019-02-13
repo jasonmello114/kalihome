2a87b42b1d8d23f053971532896d3ae1dbbbf559
root <detourstudios07@gmail.com>
2019-02-13 16:44:21 +0000

    Update

--- dht.py
+++ dht.py
@@ -11,4 +11,8 @@ import requests
 #temperature_f = round(temperature_c * 9/5 + 32, 1)
 # print(round(humidity, 1))
 #print(round(temperature_f, 1))
-print([x for x in requests.get("http://wttr.in/?0&T&Q").text.split("\n")[1].split(" ") if x][-2])
+temp_range = [x for x in requests.get("http://wttr.in/?0&T&Q").text.split("\n")[1].split(" ") if x][-2]
+if "-" in temp_range:
+	print(sum(list(map(int, temp_range.split("-")))) / 2)
+else:
+	print(temp_range)
